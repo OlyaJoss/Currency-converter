8167fb2817d4b4550348a7ad3f39ef0ab149886b
alexander <urazaev.alexander@gmail.com>
2021-07-30 14:36:12 +0500

    dataRender

--- js/script.js
+++ js/script.js
@@ -1,16 +1,21 @@
 const state = {
     buttonListLeft: document.querySelector('.converter__list--left'),
     buttonListRight: document.querySelector('.converter__list--right'),
-    initialCurrency: 'EUR',
-    secondaryCurrency: 'USD',
     selectLeft: document.querySelector('.converter__list-select'),
     selectRight: document.querySelector('.converter__list-select--right'),
-    URL: 'http://api.exchangeratesapi.io/v1/latest',
-    API_KEY: '5249425ddee3e46303c2e3aeca06484d',
+
+    initialCurrency: 'EUR',
+    secondaryCurrency: 'USD',
     rateLeftText: document.querySelector('.converter--left .converter__current-rate'),
     rateRightText: document.querySelector('.converter--right .converter__current-rate'),
     inputLeft: document.querySelector('.converter--left .converter__input'),
     inputRight: document.querySelector('.converter--right .converter__input'),
+
+    URL: 'https://www1.oanda.com/rates/api/v2/rates/spot.json',
+    API_KEY: 'sJdI0ater0rUIYOTFdUo6pY1',
+
+    rateLeftToRight: null,
+    rateRightToLeft: null,
 }
 
 state.buttonListLeft.addEventListener('click', (event) => {
@@ -37,16 +42,63 @@ state.buttonListRight.addEventListener('click', (event) => {
     }
 })
 
-const fetchData = (currency) => {
+const fetchData = async (currency) => {
     console.log(currency)
-    fetch(`${state.URL}?access_key=${state.API_KEY}&base=${state.initialCurrency}&symbols=${state.secondaryCurrency}`)
-        .then(res => res.json())
-        .then(data => {
-            console.log(data);
-            state.rateLeftText.innerHTML = `1 ${state.initialCurrency} = ${data.rates[state.secondaryCurrency]} ${state.secondaryCurrency}`
-            state.inputRight.value = parseInt(state.inputLeft.value) * Number(data.rates[state.secondaryCurrency])
-        })
+    if (state.initialCurrency === state.secondaryCurrency) {
+        // currencies are equal
+
+        // should renew 2spans + right input
+        state.rateLeftToRight = 1
+
+        // вызвать функцию перерисовки чтобы обновить данные
+        dataRender();
+
+        state.inputRight.value = state.inputLeft.value
+    } else {
+        fetch(`${state.URL}?api_key=${state.API_KEY}&base=${state.initialCurrency}&quote=${state.secondaryCurrency}`)
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                // получаем актуальный курс
+                state.rateLeftToRight = Number(data.quotes[0].midpoint)
+                // вызываем ф-ию которая считает по этому курсу и обновляет данные
+                dataRender();
+
+            })
+    }
 }
+
+// функция которая обновляет данные в инпутах и в спанах
+
+const dataRender = (newLeftInputValue, newRightInputValue, newRateLeftTextValue, newRateRightTextValue) => {
+    // принимать новые значения для 2х спанов и 2х инпутов
+
+    // обновлять значения
+
+    // кейс когда валюты равны
+    if (state.initialCurrency === state.secondaryCurrency) {
+        state.rateLeftText.innerHTML = `1 ${state.initialCurrency} = 1 ${state.secondaryCurrency}`
+        state.rateRightText.innerHTML = `1 ${state.initialCurrency} = 1 ${state.secondaryCurrency}`
+    } else {
+        // кейс когда валюты не равны
+
+        state.rateLeftText.innerHTML = `1 ${state.initialCurrency} =  ${state.rateLeftToRight} ${state.secondaryCurrency}`
+        // state.rateRightText.innerHTML = `1 ${state.secondaryCurrency} =  state.rateRightToLeft ${state.initialCurrency}`
+
+        state.inputRight.value = (parseInt(state.inputLeft.value) * Number(state.rateLeftToRight)).toFixed(2)
+    }
+
+
+}
+
+dataRender(123, 345, 678, 90);
+
+fetchData()
+
+state.inputLeft.addEventListener('input', (event) => {
+    state.inputRight.value = (parseInt(state.inputLeft.value) * state.rateLeftToRight)
+    console.log(parseInt(state.inputLeft.value), state.rateLeftToRight)
+})
 
 state.selectLeft.addEventListener('change', (event) => {
     state.buttonListLeft.querySelectorAll('.converter__list-button').forEach((el) => {
